67efa9ce982e0ea367f174c4a2875046f7ae1e8a
Z_V_kill_aZoV <74547766+BlackBaboon@users.noreply.github.com>
2023-02-15 22:42:21 +0300

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,82 @@
+use master 
+go
+drop database ЛарионовДота
+go
+create database ЛарионовДота
+go
+use ЛарионовДота
+
+create table Users
+(
+ID int primary key,
+Nickname nvarchar(20) not null,
+Surname nvarchar(20) not null,
+[Name] nvarchar(20) not null,
+Email nvarchar(50) null,
+Phonenumber nvarchar(50) null,
+Authed bit not null,
+IsAdmin bit not null
+);
+
+create table SavedAdresses
+(
+UserId int,
+Title nvarchar(20) not null,
+City nvarchar(50) not null,
+Street nvarchar(50) not null,
+House int not null,
+Building int null,
+Front int null,
+Apartament int null,
+Primary key(UserId,Title),
+FOREIGN KEY (UserId) references Users(ID)
+);
+
+create table Goods
+(
+ID int primary key,
+Title nvarchar(100) not null,
+Price decimal(20,2) not null,
+Descryption nvarchar(MAX) not null,
+);
+
+create table GoodsList
+(
+UserId int,
+GoodId int,
+Amount int not null,
+PRIMARY KEY(UserId, GoodId),
+FOREIGN KEY (UserId) references Users(ID),
+FOREIGN KEY (GoodId) references Goods(ID)
+);
+
+create table LikedList
+(
+UserId int,
+GoodId int,
+PRIMARY KEY(UserId, GoodId),
+FOREIGN KEY (UserId) references Users(ID),
+FOREIGN KEY (GoodId) references Goods(ID)
+);
+
+create table Comments
+(
+UserId int,
+GoodId int,
+Rate int not null,
+Comment int,
+PRIMARY KEY(UserId, GoodId),
+FOREIGN KEY (UserId) references Users(ID),
+FOREIGN KEY (GoodId) references Goods(ID)
+);
+
+create table Ships
+(
+ID int,
+UserId int,
+GoodId int,
+ShipDate datetime not null,
+Success bit null,
+primary key(ID,UserId,GoodId),
+Foreign key (UserId,GoodId) references GoodsList(UserId,GoodId)
+);
